e69a280ec087cfa88da356126ffae75832f2f75e
Tummeti-Sai-Ram-kumar <tummetisairamkumar1234@gmail.com>
2023-02-14 14:58:44 +0530

    first commit

--- src/components/instruct.js
+++ src/components/instruct.js
@@ -3,11 +3,11 @@ import {VscDebugBreakpointLog} from 'react-icons/vsc'
 const Instruct = () => {
     return(
         <div class="instructions">
-            <p><VscDebugBreakpointLog class='icon'></VscDebugBreakpointLog> The timer runs for 15 seconds </p>
-            <p><VscDebugBreakpointLog></VscDebugBreakpointLog> There is one treasure out of 1 - 48 numbers</p>
-            <p><VscDebugBreakpointLog></VscDebugBreakpointLog> You need to select 6 unique numbers </p>
-            <p><VscDebugBreakpointLog></VscDebugBreakpointLog> If you select less than 6 numbers , I'll select the rest from my side </p>
-            <p><VscDebugBreakpointLog></VscDebugBreakpointLog> If the treasure is found in the numbers you selected , then you won else you lost</p>
+            <p> <span><VscDebugBreakpointLog></VscDebugBreakpointLog></span> The timer runs for 15 seconds </p>
+           <p>  <span><VscDebugBreakpointLog></VscDebugBreakpointLog></span> There is one treasure out of 1 - 48 </p>
+            <p><span><VscDebugBreakpointLog></VscDebugBreakpointLog></span> You need to select 6 unique numbers </p>
+            <p><span><VscDebugBreakpointLog></VscDebugBreakpointLog></span> If you select less than 6 numbers , I'll select the rest from my side </p>
+            <p><span><VscDebugBreakpointLog></VscDebugBreakpointLog></span> If the treasure is found in the numbers you selected , then you won else you lost</p>
         </div>
     )
 }
