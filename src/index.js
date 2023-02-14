65e4941e3fc7dc95d269cee41becbbb9a53cfafa
Tummeti-Sai-Ram-kumar <tummetisairamkumar1234@gmail.com>
2023-02-14 12:35:20 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -4,15 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { BrowserRouter , Routes , Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<App></App>}></Route>
-  </Routes>
-  </BrowserRouter>
+  <App></App>
 );
 
 // If you want to start measuring performance in your app, pass a function
